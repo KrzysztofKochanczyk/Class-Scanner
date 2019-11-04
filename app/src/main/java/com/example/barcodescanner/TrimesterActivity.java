6f82ce50317508dd8be1b8288c492e732c2ad2e4
ANVazquez <aramisvazquez21@gmail.com>
2019-11-04 18:34:42 -0500

    -Almost done with the trimester page. Just adding a few things to it at the moment. -Implemented two new things in the build gradle to set up a table in the trimester page.

--- app/src/main/java/com/example/barcodescanner/TrimesterActivity.java
+++ app/src/main/java/com/example/barcodescanner/TrimesterActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class TrimesterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trimester);
+
     }
 }
