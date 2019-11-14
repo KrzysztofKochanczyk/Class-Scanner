f6a7850be0b4ec42b3fcbc56f47e5002a7e0aaea
ANVazquez <aramisvazquez21@gmail.com>
2019-11-14 09:05:24 -0500

    Finished the floating action menu

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.barcodescanner;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button scanBarcodeBtn;
-    Button scanJnumBtn;
-    Button manEnterBtn;
+    FloatingActionButton fab, fab1, fab2, fab3;
+    Animation fabOpen, fabClose, rotateF, rotateB;
+    boolean isOpen = false;
+
+
     //doing this will allow "tv1" to pass to other activities
     public static TextView tv1;
 
@@ -20,14 +26,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scanBarcodeBtn = (Button)findViewById(R.id.scanBarcodeBtn);
-        tv1 = (TextView)findViewById(R.id.tv1);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
+        fab3= (FloatingActionButton)findViewById(R.id.fab3);
 
-        scanBarcodeBtn.setOnClickListener(new View.OnClickListener() {
+        fabOpen = AnimationUtils.loadAnimation(this,R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(this,R.anim.fab_close);
+
+        rotateF = AnimationUtils.loadAnimation(this,R.anim.rotate_foward);
+        rotateB = AnimationUtils.loadAnimation(this,R.anim.rotate_backward);
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                animatedFab();
             }
         });
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+    }
+
+    private void animatedFab()
+    {
+        if(isOpen)
+        {
+            fab.startAnimation(rotateF);
+            fab1.startAnimation(fabClose);
+            fab2.startAnimation(fabClose);
+            fab3.startAnimation(fabClose);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            fab3.setClickable(false);
+
+            isOpen = false;
+        }
+        else
+        {
+            fab.startAnimation(rotateB);
+            fab1.startAnimation(fabOpen);
+            fab2.startAnimation(fabOpen);
+            fab3.startAnimation(fabOpen);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            fab3.setClickable(true);
+
+            isOpen = true;
+        }
     }
 }
