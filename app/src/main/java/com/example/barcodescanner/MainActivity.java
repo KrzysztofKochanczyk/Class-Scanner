d7b4324b6f9930446d714ab1b130889f8b24beab
ANVazquez <aramisvazquez21@gmail.com>
2019-11-16 17:28:40 -0500

    Everything is following nicely. Everything works. Just need to grab the database

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.barcodescanner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.BaseAdapter;
+import android.widget.AdapterView;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     Animation fabOpen, fabClose, rotateF, rotateB;
     boolean isOpen = false;
 
+    Toolbar toolbar;
+    ListView listView;
 
     //doing this will allow "tv1" to pass to other activities
     public static TextView tv1;
@@ -27,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //tv1 = (TextView)findViewById(R.id.tv1);
+        toolbar = (Toolbar)findViewById(R.id.studentTB);
+        listView = (ListView)findViewById(R.id.studentLV);
 
         fab = (FloatingActionButton)findViewById(R.id.fab);
         fab1 = (FloatingActionButton)findViewById(R.id.fab1);
@@ -67,6 +81,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        initToolBar();
+        setupListView();
+    }
+
+    private void initToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Class name");
+    }
+
+    private void setupListView() {
+        String[] student = getResources().getStringArray(R.array.Students);
+
+        studentAdap simpleAdap = new studentAdap(this, student);
+        listView.setAdapter(simpleAdap);
+    }
+
+    public class studentAdap extends BaseAdapter {
+
+        private Context context;
+        private LayoutInflater layoutInflater;
+        private TextView student;
+        private String[] studentArray;
+
+        public studentAdap(Context context, String[] students) {
+            context = context;
+            studentArray = students;
+            //layout Inflater allows you to load different layouts in the view
+            layoutInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return studentArray.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return studentArray[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //if current view is null it takes the present view that is available and inflates
+            //our card view into list view
+            if(convertView == null) {
+                convertView = layoutInflater.inflate(R.layout.student_card, null);
+            }
+
+            student = (TextView)convertView.findViewById(R.id.studentName);
+            student.setText(studentArray[position]);
+            return convertView;
+        }
     }
 
     private void animatedFab()
